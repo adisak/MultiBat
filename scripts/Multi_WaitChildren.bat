086e3ae480876198ad502923e2a11ac2123c04ed
adisak <adisak@gmail.com>
2024-04-21 13:44:54 -0500

    WaitChildren Fix
    Sometimes WaitChildren would return before children completed due to time to launch child process... this should help

--- scripts/Multi_WaitChildren.bat
+++ scripts/Multi_WaitChildren.bat
@@ -19,12 +19,30 @@ if "%1"=="" GOTO :loop_WaitChildren
 SET /A multi_WAITCOUNT=%1
 if %multi_WAITCOUNT% LSS 1 set multi_WAITCOUNT=1
 
+if "%2"=="0" (
+	REM No Wait
+) else if "%2"=="" (
+	REM Default Wait is 2 seconds
+	call :Wait_Seconds 2
+) else (
+	REM User Specified Wait Time
+	call :Wait_Seconds %2
+)
+
 :loop_WaitChildren
 call Multi_GetNumChildren.bat
 if %MULTI_NUM_CHILDREN% LSS %multi_WAITCOUNT% GOTO :exit_WaitChildren
-timeout /t 1 /nobreak > nul
+call :Wait_Seconds
 GOTO :loop_WaitChildren
 
 :exit_WaitChildren
 ENDLOCAL
+goto:EOF
+
+:Wait_Seconds
+if ""=="%1" (
+	timeout /t 1 /nobreak > nul
+) else (
+	timeout /t %1 /nobreak > nul
+)
 goto:EOF
